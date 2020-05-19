0340e41660d979b70701c33173271e3de1b149b4
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2020-05-20 00:50:20 +0300

    connected to DAO

--- contracts/Utopia.sol
+++ contracts/Utopia.sol
@@ -135,6 +135,12 @@ contract Utopia{
         return true;
     }
 
+    function landPrice(int256 x1, 
+        int256 y1, int256 x2, int256 y2)
+                view public returns(uint256){
+        return abs(x2-x1) * abs(y2-y1) * unitLandPrice;
+    }
+
     function abs(int256 x) view public returns (uint256) {
         return uint256(x > 0 ? x : -1*x);
     }
